4228b16c3361e864204414910c67ca3f32697c13
abinbs <abinbinusam@gmail.com>
2019-08-01 21:46:23 +0530

    Added Adapters and Updated

--- LogoQuiz/app/src/main/java/com/example/logoquiz/MainActivity.java
+++ LogoQuiz/app/src/main/java/com/example/logoquiz/MainActivity.java
@@ -2,8 +2,22 @@ package com.example.logoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+
+import com.example.logoquiz.Adapter.gridViewAnswerAdapter;
+import com.example.logoquiz.Adapter.gridViewSuggestAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public List suggestSource = new ArrayList();
+    public gridViewAnswerAdapter answerAdapter;
+    public gridViewSuggestAdapter suggestAdapter;
+
+    public Button btnSummit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
